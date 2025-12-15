4c3a3323b71902b0766e0e1bd72457cf7dbba915
Jabi <michal.jabi@gmail.com>
2025-12-15 12:39:56 +0100

    explain(L02): rules of indentifiers (naming)

--- JS-teoria-zadania/L-labolatory/L02-identifiers-rules-of.js
+++ JS-teoria-zadania/L-labolatory/L02-identifiers-rules-of.js
@@ -1,3 +1,6 @@
+// @ts-check
+// Dzieki temu komentarzowi powyżej, TS Serwer pomaga nam przy typowaniu i szukaniu błędów typowania w kodzie
+
 // Identifier to coś takiego czemu nazwę nadaje programista.
 
 
@@ -18,3 +21,52 @@ console.log(y);
 const z = 3;
 
 // z = 21;
+
+
+// zmiana typu danych jest błędem ale tylko w TS 😭
+let a = '22';
+a = 1; // to jest błąd ale w TS, zauważ że runtimeowo działa ! (PS. TypeScript nie ma nic do runtime JS... pomaga nam tylko w edytorze kodu)
+console.log(a);
+
+
+
+// identifier użyty w tym samym SCOPE (zakres, zasięg), musi mieć inną nazwę !!!
+// + JS jest case sensitive!
+let xx = 2;
+let xX = 3;
+
+// raczej tak nie piszemy zakresów, one będą same nam się tworzyć
+// jak robisz funkcję, jak używasz if/else, try/catch, pętli: for i while, oraz instukcji warunkowej switch() {}
+{
+    // to jest zakres, jeśli używamy let + const (nie zadziała dla var)
+    // var NIE MA zasięgu leksykalnego
+
+    let xx = 4;
+    let iEgsistOnlyInThisScope = 'test'
+    console.log(iEgsistOnlyInThisScope);
+
+
+    // var nie daje błędy "cannot access before intialization"
+    console.log(zzz);
+    var zzz = 3
+}
+
+//  co to znaczy że var, nie ma zasięgu leksykalnego
+// zobacz, wyciekło poza zakres:
+console.log(zzz);
+// to powyżej powinno zwróć błąd !
+// tak jak np. to tutaj (bo tutaj użyłem let/const):
+// console.log(iEgsistOnlyInThisScope);
+
+function helloWorld() {
+    let xx = 5;
+}
+
+// wywołanie funkcji:
+helloWorld()
+helloWorld()
+
+// Reguły nazwnicze, jedynie 2 znaki alfanumeryczne:
+let background_$Color = 'red'
+// numbers:
+let b3ack22groun2d_$Color2 = 'red'
