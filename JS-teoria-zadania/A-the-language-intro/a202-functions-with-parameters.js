cc5e31ee9df30ec0dcb1f0f333f0d37daf2a7a5f
Jabi <michal.jabi@gmail.com>
2025-12-15 14:30:45 +0100

    explain: arguments / parameters and WIERD parts of JS Dynamics.

--- JS-teoria-zadania/A-the-language-intro/a202-functions-with-parameters.js
+++ JS-teoria-zadania/A-the-language-intro/a202-functions-with-parameters.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 /**
  *  Funkcje mogą być deklarowane z tzw. parametrami.
  *  Są to zapisy odnoszące się do argumentów (danych) przekazanych do funkcji.
@@ -13,19 +15,53 @@
  *
  *  natomiast przesyłane dane "10" i "20" nazywamy argumentami
  */
+function callMeWithoutArgs(a, b) {
+	console.log(a);
+	console.log(b);
+	return a + b;
+}
+console.log(callMeWithoutArgs())
+console.log(undefined + undefined);
+
+console.log(123 * 'A');
+
 
 // Deklaracja:
+/**
+ * Funkcja do obliczania sumy 2 liczb.
+ * @param {number} a - 1 liczba
+ * @param {number} b - 2 liczba
+ * @returns number
+ * 
+ * JSDoc https://jsdoc.app/
+ * Fajne z @ts-check i dopełnia to @ts-check ale "kosztowne" w utrzymaniu
+ */
 function sumTwoNumbers(a, b) {
 	return a + b
 }
 
-// Wywołanie:
+// Wywołanie, podaje 2 argumenty, 10 i 20:
 const result = sumTwoNumbers(10, 20)
 
 
+
+// W momencie wywołania decyduje jakiego typu wrzucę argumenty
+console.log(sumTwoNumbers('a', 'b'));
+console.log(sumTwoNumbers(true, false));
+
+// W momencie wyołania decyduje czy w ogole i jakie argumenty wrzucę
+sumTwoNumbers();
+sumTwoNumbers(1, 2, 3, 4, 5);
+// sprawdzmy wynik:
+console.log(sumTwoNumbers())
+console.log(sumTwoNumbers(1, 2, 3, 4, 5))
+
 // #1 Zadanie:
 // Pokaż wynik dodawania 10 + 20 na ekranie
+console.log(10 + 20);
 
+console.log(result);
+console.log(sumTwoNumbers(10, 20));
 
 // #2 Zadanie:
 // Policz ile to 30 + 560
@@ -46,6 +82,19 @@ function greetings(user = 'Admin') {
 // #4 Zadanie:
 // spróbuj wywołać greetings z argumentami i bez argumentu, co się stanie ?
 
+// greetings(null); to podmieni wartość user na `null`
+greetings();
+greetings(undefined);
+
 
 // #5 Zadanie:
 // Po wykonaniu zadania #4 usuń domyślną wartość user - co się stanie ?
+
+
+
+
+//  nie używaj null, zwłaszcza przy początkowym przypisaniu wartości, stosuj taki typ jaki potem będzie przechowywała ta zmienna
+// staraj się NIE ZMIENIAĆ dynamicznie typu
+let x = 0;
+
+x = 2
