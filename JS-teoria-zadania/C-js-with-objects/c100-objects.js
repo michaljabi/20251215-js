cc48b57259596580c4a865c4561a49a25a6224a9
Jabi <michal.jabi@gmail.com>
2025-12-15 15:47:23 +0100

    explain(c100): objects initial.

--- JS-teoria-zadania/C-js-with-objects/c100-objects.js
+++ JS-teoria-zadania/C-js-with-objects/c100-objects.js
@@ -6,10 +6,40 @@
  * Na początek prosty przykład....
  */
 
+// typy proste przekazywane są przez wartości
+// a typy obiektowe - przez referencje
+// możemy mieć kilka referencji do tego samego obiektu
+
+// {} - jako obiekt, to tzw. Literał obiektowy, czyli sktócony sposób zapisu tego: new Object();
+
 // Prosty przykład obiektu:
 const user = {
-	name: 'Marek'
+	name: 'Marek',
+	age: 24,
+	isAdmin: false,
+	address: {
+		street: '',
+		no: 0,
+		postalCode: '00-000'
+	}
 };
+
+user.isAdmin = true;
+
+console.log(user.address);
+
+// to jest pusty obiekt na console.log
+console.log({});
+console.log(new Object());
+
+// jakiś konretny (wyjaśnienie pod C#)
+console.log({ name: 'Marzena'}); // Wniosek: Literał obiektowy to tzw. Syntactic Sugar (lukier składnowy) - skrócony sposób towrzenia nowego obiektu
+console.log(new Object({ name: 'Marzena'}));
+
+const user2 = user;
+
+user2.name = 'Krysia'
+// user = {};
 
 // użycie obiektu:
 console.log(user);
@@ -22,3 +52,13 @@ console.log(user['name'])
 // lub:
 const key = 'name';
 console.log(user[key])
+
+
+// winnych jezykach programowania istnieją klasy:
+class User {
+	name = 'Marek'
+}
+
+console.log(new User());
+console.log(new User());
+console.log(new User());
