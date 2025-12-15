f62174fe59d587286e93a6234536e7f6886e0adc
Jabi <michal.jabi@gmail.com>
2025-12-15 14:54:17 +0100

    task(a202-train-functions): making our own logic from 0.

--- JS-teoria-zadania/A-the-language-intro/a202-train-functions.js
+++ JS-teoria-zadania/A-the-language-intro/a202-train-functions.js
@@ -3,3 +3,15 @@
 // wywołana bez argumentu
 // wywołana z argumentem 'Michał' wyświetli napis: 'Witaj Michał'
 // Zastanów się nad poprawnymi nazwami funkcji i jej parametru
+
+function sayHello(name = 'Nieznajomy') {
+    // console.log('Witaj', name)
+    // console.log('Witaj ' + name);
+    console.log(`Witaj ${name}`);
+}
+
+// 1 bez argumentu
+sayHello();
+
+// z argumentem
+sayHello('Michał');
