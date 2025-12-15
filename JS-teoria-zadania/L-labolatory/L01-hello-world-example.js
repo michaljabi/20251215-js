56678824856c7fc0e065b04633647fe33db09c0a
Jabi <michal.jabi@gmail.com>
2025-12-15 11:04:05 +0100

    taks(L01): hello world - multiline

---
+++ JS-teoria-zadania/L-labolatory/L01-hello-world-example.js
@@ -0,0 +1,29 @@
+console.log('Hello')
+console.log("World")
+
+console.log('----------')
+
+/*
+console.log('Hello
+    World')
+console.log("Hello
+    World")
+*/
+// backticks -> tzw. Template string
+// weszło po 2015r. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+console.log(`Hello
+World`)
+
+console.log('----------')
+
+console.log('Hello\nWorld')
+
+console.log('----------')
+
+const helloWorld = 'Hello\nWorld'
+console.log(helloWorld)
+
+console.log('----------  inne przykłady')
+
+console.log(helloWorld.toLowerCase())
+console.log(helloWorld.toUpperCase())
