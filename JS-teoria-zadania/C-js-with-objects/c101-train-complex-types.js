a965f69504c9f31d9d76b8ce7f4252e1ac4f5781
Jabi <michal.jabi@gmail.com>
2025-12-16 10:12:38 +0100

    task(c101-train-complex-types): make it work - 2 items in shop.

--- JS-teoria-zadania/C-js-with-objects/c101-train-complex-types.js
+++ JS-teoria-zadania/C-js-with-objects/c101-train-complex-types.js
@@ -6,6 +6,11 @@ Zdefiniuj i przypisz obiekt shoppingCartItem z polami:
 
 	Samodzielnie wymyśl ich wartości
 */
+const shoppingCartItem = {
+	name: 'Computer',
+	price: 2000,
+	tax: 0.23
+}
 
 /* #2 Zadanie:
 Zdefiniuj i przypisz obiekt shoppingCartItem2 z polami:
@@ -18,6 +23,26 @@ Zdefiniuj i przypisz obiekt shoppingCartItem2 z polami:
 Pokaż w console.log sumy cen(price) i podatków(tax) obydwu produktów jako wartość koszyka
 */
 
+const shoppingCartItem2 = {
+	name: 'Fan',
+	price: 200,
+	tax: 0.23
+}
+
+const grossValue1 = shoppingCartItem.price + shoppingCartItem.price * shoppingCartItem.tax;
+console.log(grossValue1);
+const grossValue2 = shoppingCartItem2.price * shoppingCartItem2.tax + shoppingCartItem2.price;
+console.log(grossValue2);
+const cartValue = grossValue1 + grossValue2;
+
+console.log(cartValue);
+
 
 // #3 Wykaż, że zainicjowanie dwóch nowych pustych obiektów po porównaniu === da nam wartość false.
 // Wyjaśnij dlaczego tak się dzieje
+console.log({} === {})
+console.log({} == {})
+
+// TAK NAPRAWDĘ my robimy tak pod spodem:
+console.log(new Object() === new Object())
+console.log(new Object() == new Object())
