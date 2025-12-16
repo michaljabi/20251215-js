1cc7f9aaca94198ca25ba2951643a781a350c8a5
Jabi <michal.jabi@gmail.com>
2025-12-16 11:48:41 +0100

    task(a302): train scopes - make it work.

--- JS-teoria-zadania/A-the-language-intro/a302-train-scopes.js
+++ JS-teoria-zadania/A-the-language-intro/a302-train-scopes.js
@@ -1,6 +1,28 @@
 
-const helloWorld = "hello World!";
+const helloWorld = "hello world!";
+//const helloWorld = "";
 
+console.log(helloWorld[0])
+console.log(helloWorld[helloWorld.length-1])
+// standardowy problem JS (a jakże...)
+console.log(helloWorld[200])
+
+//  nowość 2023 rok?
+console.log(helloWorld.at(0))
+
+// console.log(undefined.toUpperCase())
+// console.log(helloWorld.at(0).toUpperCase())
+console.log(helloWorld.at(-1))
+
+
+
+// console.log(helloWorld.charAt(0))
+// console.log(helloWorld.charAt(-1))
+// Najbezpieczniejsze rozwiązanie, ponieważ .charAt - ZAWSZE zwróci string (sprawdź doc.):
+console.log(helloWorld.charAt(0))
+console.log(helloWorld.charAt(0).toUpperCase())
+console.log(helloWorld.charAt(200))
+console.log(helloWorld.charAt(helloWorld.length - 1))
 
 // #1 Zadanie:
 // Przygotuj funkcję a następnie wywołaj ją
@@ -8,6 +30,17 @@ const helloWorld = "hello World!";
 // pierwszy i ostatni znak stringa = 'h!'
 // pomocna dłoń?
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+
+function callMeNow() {
+    console.log(helloWorld.at(0) + helloWorld.at(-1))
+    let x = 10;
+    function printMyNumber() {
+        console.log(x + 400);
+        console.log(helloWorld);
+    }
+    printMyNumber();
+}
+callMeNow();
 
 // #2 Zadanie:
 // Dopisz w środku funkcji zmienną lokalną x = 10
