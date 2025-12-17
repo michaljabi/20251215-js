f53285050e8023c242cbc345609e73e50033a63f
Jabi <michal.jabi@gmail.com>
2025-12-17 14:32:46 +0100

    task(first): use ESM syntax

--- JS-teoria-zadania/B-modular-js-in-node/program-1/first.mjs
+++ JS-teoria-zadania/B-modular-js-in-node/program-1/first.mjs
@@ -1,5 +1,24 @@
-import sayHelloWorld from "./say-hello-world.mjs";
+import { Car } from "../program-2/Car.mjs";
+import hello, { myText, myOtherNumber } from "./say-hello-world.mjs"; //rozszerzenie .mjs jest CELOWE - żeby zadziałało w Node.js bez użycia `package.json`
 
-sayHelloWorld.sayHello();
+// Osobne pliki w JS nazywają się "Modułami".
+// Składnia import/export nazywa się ESM
+// co rozwija się Ecma Script Modules.
 
-console.log('Your lucky number is', sayHelloWorld.LUCKY_NUMBER)
+console.log(hello);
+
+//console.log(hello === sayHelloWorld);
+console.log(myText);
+
+console.log(myOtherNumber);
+
+hello.sayHello();
+
+console.log('Your lucky number is', hello.LUCKY_NUMBER)
+
+
+// Zaimportuj do tego pliku, klasę Car
+// zrób za jej pomocą instancję car czyli (const car = new Car() )
+// pokaż instancję na konsoli ../
+const car = new Car();
+console.log(car);
--- JS-teoria-zadania/B-modular-js-in-node/program-1/say-hello-world.mjs
+++ JS-teoria-zadania/B-modular-js-in-node/program-1/say-hello-world.mjs
@@ -1,9 +1,14 @@
 console.log('To jest załadowana część programu')
 
+export const myOtherNumber = 2000;
+export const myText = 'hello';
 
-export default {
-	sayHello: function() {
+const sayHelloWorld =  {
+	sayHello() {
 		console.log('Hello world')
 	},
 	LUCKY_NUMBER: 7
 }
+
+export default sayHelloWorld;
+// export default sayHelloWorld;
