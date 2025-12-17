33a004183c3fa30c568193bd1ef762bc6589dbb2
Jabi <michal.jabi@gmail.com>
2025-12-17 10:44:41 +0100

    task(a401): make it work.

--- JS-teoria-zadania/A-the-language-intro/a401-train-loops.js
+++ JS-teoria-zadania/A-the-language-intro/a401-train-loops.js
@@ -4,9 +4,21 @@
 // 2
 // 3
 // 4
+for(let x = 1; x <= 10; x++) {
+    console.log(x);
+}
 
 
 // #2 Zadanie
 // Pokaż na konsoli ciąg liczb 1 do 10 jedna obok drugiej:
 // 1 2 3 4 5 6 7 8 9 10
-
+let oneLine = '';
+for(let x = 1; x <= 10; x++) {
+    if(x === 1) {
+        oneLine += x;
+        continue;
+    } 
+    oneLine += ' ' + x;
+}
+// oneLine = oneLine.trim();
+console.log(oneLine);
